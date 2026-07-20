a6b1b2416523d821a3c4804fa400d86bc6dce609
Noahmathieu <noahtown011@gmail.com>
2026-07-20 16:59:41 +0300

    it

--- codeigniter4-framework-b3359be/app/Models/HistoriqueModel.php
+++ codeigniter4-framework-b3359be/app/Models/HistoriqueModel.php
@@ -10,15 +10,6 @@ class HistoriqueModel extends Model
     protected $useTimestamps = false;
      protected $allowedFields = ['client_id', 'type_operation_id', 'montant','commission', 'date_operation','frais','destinataire'];
 
-
-    public function findbyIdClient($clientId)
-    {
-        return $this->select('historique.*, type_operation.nomType as typeOperationNom')
-                    ->join('type_operation', 'type_operation.id = historique.type_operation_id')
-                    ->where('client_id', $clientId)
-                    ->findAll();
-    }
-
     public function findbyIdClient($clientId)
     {
         return $this->join('type_operation', 'type_operation.id = historique.type_operation_id')
